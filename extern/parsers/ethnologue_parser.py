2ac1c66c1f58198b0f46b30f8299c2bc5e68ca69
Katalin <pajkossyk@gmail.com>
2014-02-21 17:26:37 +0100

    EthnologueParser, LangueArchivesParser classes 	modified:   ethnologue_parser.py 	modified:   language_archives_parser.py

--- extern/parsers/ethnologue_parser.py
+++ extern/parsers/ethnologue_parser.py
@@ -3,114 +3,117 @@ import urllib2
 
 from base_parsers import OnlineParser
 
-def strip_nonstring(string):
+class EthnologueParser(OnlineParser):
 
-    while len(string) > 0 and string[0] == '<':
-        string = string.split('>')[1].split('<')[0]
-    return string
+    def __init__(self, sil_code):
+	self.sil_code = sil_code
+	self.url = 'http://www.ethnologue.com/language/{0}'.format(sil_code)
 
-def get_value_dictionary(lista):
-
-    dictionary = {}
-    for item in lista:
-        key = item.split('>')[1].split('</')[0].strip()
-        values = item.split('<span class="field-content">')
-        if len(values) > 1:
-            value = item.split('<span class="field-content">')[1].split(
-		    '</span>')[0].strip()
-        else:
-            value = item.split('<div class="field-content">')[1].split(
-		    '</div>')[0].strip()
-        dictionary[key] = value
-    return dictionary 
-
-def parse_attachement(attachement_rows):
-
-    dictionary = {}
-    for row in attachement_rows:
-        key_extras = row.split('<div class="field-content">'
-		)[1].split('</div>')[0]
-        key = strip_nonstring(key_extras).strip()
-        value_list = row.split("<strong class=")[1:] 
-        value_dictionary = get_value_dictionary(value_list)
-        dictionary[key] = value_dictionary
-    return dictionary    
-
-def parse_rows(table):
-
-    dictionary = {}
-    for row in table:
-        key = row.split('</div>')[0].strip()
-        value_wrapped = '</div>'.join(row.split('</div>')[1:])
-        if len(value_wrapped.split('<div class="field-item even">')) > 1:
-            value_extras = value_wrapped.split('<div class="field-item even">'
-		    )[1].split('</div>')[0]
-        else:
-            value_extras = value_wrapped.split('<div class="field-item">'
-		    )[1].split('</div>')[0]
-        value = strip_nonstring(value_extras).strip()
-        dictionary[key] = value
-    return dictionary    
-
-def get_dictionary(string):
-
-    title = string.split('<h1 class="title" id="page-title">')[1]\
-	    .split('</h1>')[0]
-    country_whole = string.split('<h2>')[1].split('>')[1]
+    def strip_nonstring(self, string):
     
-    country = country_whole.split('</a')[0]
-    first_table_rows = string.split('<div class="field-label">')[1:-1]\
-    + [string.split('<div class="field-label">')[-1]\
-    .split('<div class="attachment attachment-after">')[0]]
-    dictionary = parse_rows(first_table_rows)
-    dictionary['Name'] = title
-    dictionary['Country'] = country
-    attachement = string.split('<div class="attachment attachment-after">')\
-    [1].split('<aside class="grid-6 region region-sidebar-second"id="\
-    region-sidebar-second">')[0]
-
-    if  '<div class="view-header">' in attachement:
-        attachement_title = attachement.split('<h3>')[1].split('</h3>')[0]
-        attachement_rows = attachement.split('<div class=\
-        "views-field views-field-field-country views-accordion-header">')[1:]
-        dictionary_2 = parse_attachement(attachement_rows) 
-        dictionary[attachement_title] = dictionary_2
-    return dictionary
-
-def dictionary_print(dictionary, depth):
-
-    for key in dictionary:
-        string = ''
-        for i in xrange(depth):
-            string = string + '\t'
-        if type(dictionary[key]) == dict:
-            
-            print string + key 
-            dictionary_print(dictionary[key], depth + 1)
-        else:
-            print string + key + ':\t ' + dictionary[key]     
-
-def get_ethnologue(sil_code):
+        while len(string) > 0 and string[0] == '<':
+            string = string.split('>')[1].split('<')[0]
+        return string
     
-    url = 'http://www.ethnologue.com/language/{0}'.format(sil_code)
-    parser = OnlineParser()
-    try:
-	response = urllib2.urlopen(url)
-    except:
-        sys.stderr.write('Error while downloading {0}\n'.format(url))
-	quit()
-    try:	
-        html = response.read()
-        dictionary = get_dictionary(html)
-	return dictionary
-    except:	
-        sys.stderr.write('Error while parsing {0}\n'.format(url))
+    def get_value_dictionary(self, list_):
+    
+        dictionary = {}
+        for item in list_:
+            key = item.split('>')[1].split('</')[0].strip()
+            values = item.split('<span class="field-content">')
+            if len(values) > 1:
+                value = item.split('<span class="field-content">')[1].split(
+    		    '</span>')[0].strip()
+            else:
+                value = item.split('<div class="field-content">')[1].split(
+    		    '</div>')[0].strip()
+            dictionary[key] = value
+        return dictionary 
+    
+    def parse_attachement(self, attachement_rows):
+    
+        dictionary = {}
+        for row in attachement_rows:
+            key_extras = row.split('<div class="field-content">'
+    		)[1].split('</div>')[0]
+            key = self.strip_nonstring(key_extras).strip()
+            value_list = row.split("<strong class=")[1:] 
+            value_dictionary = self.get_value_dictionary(value_list)
+            dictionary[key] = value_dictionary
+        return dictionary    
+    
+    def parse_rows(self, table):
+    
+        dictionary = {}
+        for row in table:
+            key = row.split('</div>')[0].strip()
+            value_wrapped = '</div>'.join(row.split('</div>')[1:])
+            if len(value_wrapped.split('<div class="field-item even">')) > 1:
+                value_extras = value_wrapped.split('<div class="field-item even">'
+    		    )[1].split('</div>')[0]
+            else:
+                value_extras = value_wrapped.split('<div class="field-item">'
+    		    )[1].split('</div>')[0]
+            value = self.strip_nonstring(value_extras).strip()
+            dictionary[key] = value
+        return dictionary    
+    
+    def get_dictionary(self, string):
+    
+        title = string.split('<h1 class="title" id="page-title">')[1]\
+    	    .split('</h1>')[0]
+        country_whole = string.split('<h2>')[1].split('>')[1]
+        
+        country = country_whole.split('</a')[0]
+        first_table_rows = string.split('<div class="field-label">')[1:-1]\
+        + [string.split('<div class="field-label">')[-1]\
+        .split('<div class="attachment attachment-after">')[0]]
+        dictionary = self.parse_rows(first_table_rows)
+        dictionary['Name'] = title
+        dictionary['Country'] = country
+        attachement = string.split('<div class="attachment attachment-after">')\
+        [1].split('<aside class="grid-6 region region-sidebar-second"id="\
+        region-sidebar-second">')[0]
+    
+        if  '<div class="view-header">' in attachement:
+            attachement_title = attachement.split('<h3>')[1].split('</h3>')[0]
+            attachement_rows = attachement.split('<div class=\
+            "views-field views-field-field-country views-accordion-header">')[1:]
+            dictionary_2 = self.parse_attachement(attachement_rows) 
+            dictionary[attachement_title] = dictionary_2
+        return dictionary
+    
+    def dictionary_print(self, dictionary, depth):
+    
+        for key in dictionary:
+            string = ''
+            for i in xrange(depth):
+                string = string + '\t'
+            if type(dictionary[key]) == dict:
+                print string + key 
+                self.dictionary_print(dictionary[key], depth + 1)
+            else:
+                print string + key + ':\t ' + dictionary[key]     
+    
+    def get_attributes(self):
+        try:
+    	    response = urllib2.urlopen(self.url)
+        except:
+            sys.stderr.write('Error while downloading {0}\n'.format(self.url))
+    	    quit()
+        try:	
+            html = response.read()
+            dictionary = self.get_dictionary(html)
+    	    return dictionary
+        except:	
+            sys.stderr.write('Error while parsing {0}\n'.format(self.url))
 
 def main():
 
     sil_code = sys.argv[1]
-    dictionary = get_ethnologue(sil_code)
-    dictionary_print(dictionary, 0)
+    parser = EthnologueParser(sil_code)
+    dictionary = parser.get_attributes()
+    parser.dictionary_print(dictionary, 0)
 
 if __name__ == "__main__":
     main()
--- extern/parsers/language_archives_parser.py
+++ extern/parsers/language_archives_parser.py
@@ -3,82 +3,88 @@ import urllib2
 
 from base_parsers import OnlineParser
 
-def parse_table(table):
+class LanguageArchivesParser(OnlineParser):
 
-    dictionary = {} 
-    rows = table.split('<li>')[1:]
-    dictionary['All'] = len(rows)
-    online_count = 0
-    for row in rows:
-        if '<span class="online_indicator">' in row:
-            online_count += 1
-    dictionary['Online'] = online_count
+    def __init__(self, sil_code):
+	self.sil_code = sil_code
+	self.url = 'http://www.language-archives.org/language/{0}'\
+		.format(self.sil_code) 
 
-    return dictionary
-
-def parse_table_list(table_list):
-
-    dictionary = {}
-    for table in table_list:
-        name = table.split('</h2>')[0]
-        info_dict = parse_table(table.split('<ol>')[1].split('</ol>')[0])
-        dictionary[name] = info_dict 
+    def parse_table(self, table):
     
-    return dictionary
-
-def get_dictionary(string):
-  
-    dictionary = {}
-    name_wrapped = string.split('about the')[1].split('</title>')[0]
-    if 'language'in name_wrapped:
-        name = name_wrapped.split('language')[0].strip(' ')
-    else:
-        name = name_wrapped
-    code = string.split('<p>ISO 639-3:')[1].split('>')[1].split('</a')[0]\
-	    .strip()
-    dictionary_item_inputs = string.split('<h2>')[1:] 
-    dictionary = parse_table_list(dictionary_item_inputs)
-    dictionary['Name'] = name
-    dictionary['Code'] = code
+        dictionary = {} 
+        rows = table.split('<li>')[1:]
+        dictionary['All'] = len(rows)
+        online_count = 0
+        for row in rows:
+            if '<span class="online_indicator">' in row:
+                online_count += 1
+        dictionary['Online'] = online_count
     
-    return dictionary
-
-def dictionary_print(dictionary):
-
-    list_ = ['Name', 'Code', 'Primary texts', 'Lexical resources',\
-	    'Language descriptions', 'Other resources about the language',\
-	    'Other resources in the language'] 
-
-    string = '{0}\t{1}'.format(dictionary['Name'], dictionary['Code'])
-    for item in lista[2:]:
-        if item in dictionary:
-	    string += "\t{0}\t{1}".format(dictionary[item]['All'],\
-		    dictionary[item]['Online'])
+        return dictionary
+    
+    def parse_table_list(self, table_list):
+    
+        dictionary = {}
+        for table in table_list:
+            name = table.split('</h2>')[0]
+            info_dict = self.parse_table(table.split('<ol>')[1].split('</ol>')[0])
+            dictionary[name] = info_dict 
+        
+        return dictionary
+    
+    def get_dictionary(self, string):
+      
+        dictionary = {}
+        name_wrapped = string.split('about the')[1].split('</title>')[0]
+        if 'language'in name_wrapped:
+            name = name_wrapped.split('language')[0].strip(' ')
         else:
-            string +=  '\t{0}\t{0}'.format(0)
-    print string
-  
-def get_lang_archive(sil_code):
+            name = name_wrapped
+        code = string.split('<p>ISO 639-3:')[1].split('>')[1].split('</a')[0]\
+    	    .strip()
+        dictionary_item_inputs = string.split('<h2>')[1:] 
+        dictionary = self.parse_table_list(dictionary_item_inputs)
+        dictionary['Name'] = name
+        dictionary['Code'] = code
+        
+        return dictionary
     
-    url = 'http://www.language-archives.org/language/{0}'.format(sil_code)
-    parser = OnlineParser()
-    try:
-	response = urllib2.urlopen(url)
-    except:
-        sys.stderr.write('Error while downloading {0}\n'.format(url))
-	quit()
-    try:	
-        html = response.read()
-        dictionary = get_dictionary(html)
-	return dictionary
-    except:	
-        sys.stderr.write('Error while parsing {0}\n'.format(url))
+    def dictionary_print(self, dictionary):
+    
+        list_ = ['Name', 'Code', 'Primary texts', 'Lexical resources',\
+    	    'Language descriptions', 'Other resources about the language',\
+    	    'Other resources in the language'] 
+    
+        string = '{0}\t{1}'.format(dictionary['Name'], dictionary['Code'])
+        for item in list_[2:]:
+            if item in dictionary:
+    	        string += "\t{0}\t{1}".format(dictionary[item]['All'],\
+    		    dictionary[item]['Online'])
+            else:
+                string +=  '\t{0}\t{0}'.format(0)
+        print string
+      
+    def get_attributes(self):
+        
+        try:
+    	    response = urllib2.urlopen(self.url)
+        except:
+            sys.stderr.write('Error while downloading {0}\n'.format(self.url))
+    	    quit()
+        try:	
+            html = response.read()
+            dictionary = self.get_dictionary(html)
+    	    return dictionary
+        except:	
+            sys.stderr.write('Error while parsing {0}\n'.format(self.url))
 
 def main():
 
     sil_code = sys.argv[1]
-    dictionary = get_lang_archive(sil_code)
-    dictionary_print(dictionary)
+    parser = LanguageArchivesParser(sil_code)
+    dictionary = parser.get_attributes()
+    parser.dictionary_print(dictionary)
 
 if __name__ == "__main__":
     main()
