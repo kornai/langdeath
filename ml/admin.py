53703fff602d564331c61b47f2eeb6b7ab746cf9
Judit Ács <judit@sch.bme.hu>
2014-03-02 11:42:47 +0100

    added resource to admin

--- ml/admin.py
+++ ml/admin.py
@@ -3,9 +3,22 @@ from django.contrib import admin
 from ml import models
 
 
+class ResourceAuthorshipInline(admin.StackedInline):
+    model = models.ResourceAuthorship
+    extra = 3
+
+
+class ResourceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['name', 'description', 'url']}),
+    ]
+    inlines = [ResourceAuthorshipInline]
+
+
 class PublicationAuthorshipInline(admin.StackedInline):
     model = models.PublicationAuthorship
     extra = 3
+
 
 class PublicationAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -13,6 +26,9 @@ class PublicationAdmin(admin.ModelAdmin):
     ]
     inlines = [PublicationAuthorshipInline]
 
+
+admin.site.register(models.Resource, ResourceAdmin)
+admin.site.register(models.ResourceAuthorship)
 admin.site.register(models.Publication, PublicationAdmin)
-admin.site.register(models.UserProfile)
 admin.site.register(models.PublicationAuthorship)
+admin.site.register(models.UserProfile)
