6e41b82be5a7c8f9dfbf68b5f1518139902752e4
Judit Acs <judit@sch.bme.hu>
2014-03-03 11:06:17 +0100

    rearranged static files. Removing duplicates

--- langdeath/settings.py
+++ langdeath/settings.py
@@ -82,8 +82,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Settings added by us
 
 AUTH_PROFILE_MODULE = 'ml.UserProfile'
