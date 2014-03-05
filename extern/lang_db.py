d2fba0f7724a20e58dab0f5a6643db8451ba6d08
Attila Zseder <zseder@gmail.com>
2014-03-05 12:27:26 +0100

    modified:   extern/lang_db.py pep8

--- extern/lang_db.py
+++ extern/lang_db.py
@@ -1,7 +1,9 @@
 from dld.models import Language
 
+
 class LanguageUpdate(object):
     pass
+
 
 class LanguageDB(object):
     def __init__(self):
@@ -11,21 +13,21 @@ class LanguageDB(object):
         """Inserts new language to db
         """
         if not isinstance(lang, LanguageUpdate):
-            raise TypeError("LanguageDB.add_new_language " + 
-                "got non-LanguageUpdate instance")
+            raise TypeError("LanguageDB.add_new_language " +
+                            "got non-LanguageUpdate instance")
 
         # TODO add language to db
         # new Language instance from LanguageUpdate instance
-    
+
     def update_lang_data(self, tgt, update):
         """Updates data for @tgt language
         """
         if not isinstance(update, LanguageUpdate):
-            raise TypeError("LanguageDB.update_lang_data " + 
-                "got non-LanguageUpdate instance as @update")
+            raise TypeError("LanguageDB.update_lang_data " +
+                            "got non-LanguageUpdate instance as @update")
 
         if not isinstance(tgt, Language):
-            raise TypeError("LanguageDB.update_lang_data " + 
-                "got non-Language instance as @tgt")
+            raise TypeError("LanguageDB.update_lang_data " +
+                            "got non-Language instance as @tgt")
 
         # TODO update tgt's data with @update
