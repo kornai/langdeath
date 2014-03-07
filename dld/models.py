5d653f1cf56597e8e3e7f602d070fec0f2d4f944
Attila Zseder <zseder@gmail.com>
2014-03-07 11:39:52 +0100

    modified:   ../../dld/models.py local_name

--- dld/models.py
+++ dld/models.py
@@ -5,9 +5,8 @@ from jsonfield import JSONField
 class Language(models.Model):
 
     name = models.CharField(max_length=100)
-    name2= models.CharField(max_length=100)
+    local_name = models.CharField(max_length=100)
     sil = models.CharField(max_length=10, primary_key=True)
     last_updated = models.DateTimeField('last updated')
     l1_speakers = JSONField()
-
-
+    other_codes = JSONField()
