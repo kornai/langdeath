9f8cdd971caaef6d1140cc42eb71bcd0e75b209b
makrai <makraimarton@gmail.com>
2014-03-10 16:26:41 +0100

    in_omniglot works

--- extern/ld/parsers/omniglot_parser.py
+++ extern/ld/parsers/omniglot_parser.py
@@ -3,27 +3,37 @@ from HTMLParser import HTMLParser
 
 from base_parsers import OnlineParser
 from iso_639_3_parser import LanguageUpdate
-# TODO import name_to_code
+
+def name_to_code(ref_name):
+    # TODO import this function from somewhere
+    return ref_name
+
 
 class OmniglotHTMLParser(HTMLParser):
     def __init__(self):
-        HTMLParser.HTMLParser.__init__(self)
+        HTMLParser.__init__(self)
         self.in_list = False
+        self.in_name = False
+        self.lang_updates = []
 
     def handle_starttag(self, tag, attrs):
         if tag == 'ol':
             self.in_list = True
+        elif self.in_list and tag == 'a':
+            self.in_name = True
 
-    def handle_endtag(self, tag, attrs):
+    def handle_endtag(self, tag):
         if tag == 'ol':
             self.in_list = False
+        elif tag == 'a':
+            self.in_name = False
 
     def handle_data(self, data):
-        if self.in_list:
+        if self.in_name:
             lang_ud = LanguageUpdate()
             lang_ud.code = name_to_code(data)
             lang_ud.in_omniglot = True
-            yield lang_ud
+            self.lang_updates.append(lang_ud)
 
 
 class OmniglotParser(OnlineParser):
@@ -32,6 +42,7 @@ class OmniglotParser(OnlineParser):
         self.url = 'http://www.omniglot.com/writing/languages.htm'
         
     def parse(self):
-        html_filen, headers = urllib.urlretrieve(self.url)
         html_parser = OmniglotHTMLParser()
-        return html_parser.feed(open(html_filen).read())
+        html_filen, headers = urllib.urlretrieve(self.url)
+        html_parser.feed(open(html_filen).read())
+        return iter(html_parser.lang_updates)
