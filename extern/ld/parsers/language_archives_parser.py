ad8208d6838250c419a8662ff02c73d3fbd4abc6
Katalin <pajkossyk@gmail.com>
2014-03-17 15:56:20 +0100

    flek8, logging.debug -> langdetah_exceptions

--- extern/ld/parsers/language_archives_parser.py
+++ extern/ld/parsers/language_archives_parser.py
@@ -3,11 +3,12 @@ import urllib2
 import logging
 
 from base_parsers import OnlineParser
+from ld.langdeath_exceptions import LangdeathException
 
 class LanguageArchivesParser(OnlineParser):
 
     def parse_table(self, item):
-        
+
         try:
             table = item.split('<ol>')[1].split('</ol>')[0]
             rows = table.split('<li>')[1:]
@@ -15,11 +16,12 @@ class LanguageArchivesParser(OnlineParser):
             for row in rows:
                 if '<span class="online_indicator">' in row:
                     online_count += 1
-            return len(rows), online_count        
+            return len(rows), online_count
         except Exception as e:
-            logging.debug('{0} in LanguageArchivesParser.parse_table'
-                          .format(type(e)))
-    
+            raise LangdeathException(
+                '{0} in LanguageArchivesParser.parse_table'
+                    .format(type(e)))
+
     def get_tabular_data(self, html):
 
         try:
@@ -31,8 +33,9 @@ class LanguageArchivesParser(OnlineParser):
                 d[category] = counts
             return d
         except Exception as e:
-            logging.debug('{0} in LanguageArchivesParser.get_tabular_data'
-                          .format(type(e)))
+            raise LangdeathException(
+                '{0} in LanguageArchivesParser.get_tabular_data'
+                    .format(type(e)))
 
     def get_name(self, string):
 
@@ -44,34 +47,37 @@ class LanguageArchivesParser(OnlineParser):
                 name = name_wrapped
             return name
         except Exception as e:
-            logging.debug('{0} in LanguageArchivesParser.get_name'\
-                          .format(type(e)))
+            raise LangdeathException(
+                '{0} in LanguageArchivesParser.get_name'
+                    .format(type(e)))
 
     def parse(self, sil_codes):
-        
+
         for sil in sil_codes:
             html = self.get_html(sil)
             dictionary = {}
             dictionary['Name'] = self.get_name(html)
             d = self.get_tabular_data(html)
-            if d != None:
+            if d is not None:
                 for key in d:
                     dictionary[key] = {}
                     all_, online = d[key]
                     dictionary[key]['All'] = all_
                     dictionary[key]['Online'] = online
-            yield dictionary        
+            yield dictionary
 
     def get_html(self, sil_code):
-        
+
         url = 'http://www.language-archives.org/language/{0}'\
-                .format(sil_code)
+            .format(sil_code)
         try:
-    	    response = urllib2.urlopen(url)
+            response = urllib2.urlopen(url)
             html = response.read()
             return html
         except:
-            logging.debug('Error while downloading {0}\n'.format(url))
+            raise LangdeathException(
+                'Error while downloading {0}\n'.format(url))
+
 
 def main():
 
