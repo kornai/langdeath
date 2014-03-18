e8ca8ae7ce0ab8cb990bf596c32d3b6c39a8db75
Katalin <pajkossyk@gmail.com>
2014-03-18 16:36:54 +0100

    new file:   wikipedia_incubators_parser.py

---
+++ extern/ld/parsers/wikipedia_incubators_parser.py
@@ -0,0 +1,124 @@
+import urllib2
+import re
+
+from base_parsers import OnlineParser
+from ld.langdeath_exceptions import LangdeathException
+
+
+class WikipediaIncubatorsParser(OnlineParser):
+
+    def generate_rows(self, tabular):
+        pattern = re.compile('<tr.*?>(.*?)</tr>(.*)', re.DOTALL)
+        try:
+            m = pattern.search(tabular)
+            while m is not None:
+                row, rest = m.groups()
+                yield row
+                m = pattern.search(rest)
+        except Exception as e:
+            raise LangdeathException(
+                '{0} in LanguageArchivesParser.generate_rows'
+                .format(type(e)))
+
+    def get_tabular(self, html):
+
+        try:
+            pattern = re.compile('<table class=.*?>(.*?)</table>', re.DOTALL)
+            m = pattern.search(html)
+            return m.groups()[0]
+        except Exception as e:
+            raise LangdeathException(
+                '{0} in LanguageArchivesParser.get_tabular'
+                    .format(type(e)))
+
+    def replace_html_formatting(self, row):
+
+        row = re.sub('<[/]{0,1}a.*?>', '', row)
+        row = re.sub('<[/]{0,1}b>', '', row)
+        return row
+
+    def split_row(self, row):
+
+        try:
+            row = row.replace('\n', '')
+            row = self.replace_html_formatting(row)
+            return [item.strip('</td> ')
+                    for item in re.split('<td.*?>', row)[1:]]
+        except Exception as e:
+            raise LangdeathException(
+                '{0} in LanguageArchivesParser.split_row'
+                .format(type(e)))
+
+    def get_row_dict(self, column_titles, cells):
+
+        try:
+            return dict([(column_titles[i], cells[i])
+                         for i in xrange(len(column_titles))])
+        except Exception as e:
+            raise LangdeathException(
+                '{0} in LanguageArchivesParser.get_row_dict'
+                .format(type(e)))
+
+    def split_headline(self, headline):
+        main_headline = headline.split('<tr>')[-2]
+        main_headline = re.sub('(</tr>|\n|</th>)', '', main_headline)
+        return re.split('<th.*?>', main_headline)[1:]
+
+    def strip_header(self, tabular_):
+        pattern = re.compile('<th(.*?)(<td.*)', re.DOTALL)
+        try:
+            header, rest = pattern.search(tabular_).groups()
+            return header, rest
+        except Exception as e:
+            raise LangdeathException(
+                '{0} in LanguageArchivesParser.strip_header'
+                .format(type(e)))
+
+    def get_wp_tabular(self, tabular_):
+        pattern = re.compile('<span class=.*?>Wikipedia</span>.*?(<td.*?)' +
+                             '<tr>[\n]{0,1}<td.*?editsection', re.DOTALL)
+        try:
+            wp_tabular = pattern.search(tabular_).groups()[0]
+            return wp_tabular
+        except Exception as e:
+            raise LangdeathException(
+                '{0} in LanguageArchivesParser.get_wp_tabular'
+                .format(type(e)))
+
+    def generate_dictionaries(self, string):
+
+        tabular_ = self.get_tabular(string)
+        headline, tabular = self.strip_header(tabular_)
+        column_titles = self.split_headline(headline)
+        wp_tabular = self.get_wp_tabular(tabular_)
+        for row in self.generate_rows(wp_tabular):
+            cells = self.split_row(row)
+            row_dict = self.get_row_dict(column_titles, cells)
+            yield row_dict
+
+    def parse(self):
+
+        html = self.get_html()
+        for dict_ in self.generate_dictionaries(html):
+            yield dict_
+
+    def get_html(self):
+
+        url = 'http://incubator.wikimedia.org/wiki/Incubator:Wikis'
+        try:
+            response = urllib2.urlopen(url)
+            html = response.read().decode('utf-8')
+            return html
+        except:
+            raise LangdeathException(
+                'Error while downloading {0}\n'.format(url))
+
+
+def main():
+
+    parser = WikipediaIncubatorsParser()
+    for d in parser.parse():
+        print repr(d)
+
+if __name__ == "__main__":
+    main()
