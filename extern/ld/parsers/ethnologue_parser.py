e69baab58c5a0b95b6a3003503fe4c00b99adbfe
Attila Zseder <zseder@gmail.com>
2014-04-10 12:07:16 +0200

    modified:   extern/ld/parsers/ethnologue_parser.py pep8

--- extern/ld/parsers/ethnologue_parser.py
+++ extern/ld/parsers/ethnologue_parser.py
@@ -2,7 +2,7 @@ import sys
 import re
 
 from base_parsers import OnlineParser
-from ld.langdeath_exceptions import ParserException 
+from ld.langdeath_exceptions import ParserException
 from utils import get_html
 
 
@@ -18,12 +18,12 @@ class EthnologueParser(OnlineParser):
             string = string.split('>')[1].split('<')[0]
         return string
 
-    def parse_attachement_block(self, block):
+    def parse_attachment_block(self, block):
         try:
             inner_dictionary = {}
             for item in re.split('<strong class=.*?>', block)[1:]:
-                matcher = \
-                re.compile('(.*?)</strong><span class=.*?>(.*?)</span>')
+                matcher = re.compile(
+                    '(.*?)</strong><span class=.*?>(.*?)</span>')
                 matched = matcher.match(item)
                 if matched is not None:
                     key, value = matched.groups()
@@ -31,7 +31,7 @@ class EthnologueParser(OnlineParser):
             return inner_dictionary
         except Exception as e:
             raise ParserException(
-            '{0} in EthnologueParser.parse_attachement_block()' +
+                '{0} in EthnologueParser.parse_attachment_block()' +
                 ' sil:{1}'.format(type(e), self.sil))
 
     def parse_row(self, row):
@@ -42,14 +42,14 @@ class EthnologueParser(OnlineParser):
                 value_extras = value_wrapped.split(
                     '<div class="field-item even">')[1].split('</div>')[0]
             else:
-                value_extras = value_wrapped.split('<div class="field-item">'
-                )[1].split('</div>')[0]
+                value_extras = value_wrapped.split(
+                    '<div class="field-item">')[1].split('</div>')[0]
             value = self.strip_nonstring(value_extras).strip()
             return key, value
         except Exception as e:
             raise ParserException(
                 '{0} in EthnologueParser.parse_row(), at row\n{1} ' +
-                    'sil:{2}'.format(type(e), row, self.sil))
+                'sil:{2}'.format(type(e), row, self.sil))
 
     def get_title(self, string):
         try:
@@ -57,105 +57,115 @@ class EthnologueParser(OnlineParser):
                 .split('</h1>')[0]
         except Exception as e:
             raise ParserException(
-            '{0} in EthnologueParser.get_title() sil:{1}'
-                    .format(type(e), self.sil))
+                '{0} in EthnologueParser.get_title() sil:{1}'.format(
+                    type(e), self.sil))
 
     def get_country(self, string):
         try:
             return string.split('<h2>')[1].split('>')[1].split('</a')[0]
         except Exception as e:
             raise ParserException(
-            '{0} in EthnologueParser.get_country(), sil:{1}'
-                .format(type(e), self.sil))
+                '{0} in EthnologueParser.get_country(), sil:{1}'.format(
+                    type(e), self.sil))
 
     def process_main_table_rows(self, string):
         try:
             main_rows = string.split('<div class="field-label">')[1:-1]\
-                + [string.split('<div class="field-label">')[-1]
-                .split('<div class="attachment attachment-after">')[0]]
+                + [string.split('<div class="field-label">')[-1].split(
+                    '<div class="attachment attachment-after">')[0]]
             res = []
             for row in main_rows:
                 res.append(self.parse_row(row))
             return res
         except Exception as e:
             raise ParserException(
-            '{0} in EthnologueParser.process_main_table_rows()' +
+                '{0} in EthnologueParser.process_main_table_rows()' +
                 ' sil:{1}'.format(type(e), self.sil))
 
-    def get_attachement_title(self, attachement):
+    def get_attachment_title(self, attachment):
         try:
-            if not '<div class="view-header">' in attachement:
+            if not '<div class="view-header">' in attachment:
                 return None
             else:
-                attachement_title = attachement.split('<h3>')[1]\
-                        .split('</h3>')[0]
-                return attachement_title
+                attachment_title = attachment.split('<h3>')[1].split(
+                    '</h3>')[0]
+                return attachment_title
         except Exception as e:
             raise ParserException(
-            '{0} in EthnologueParser.get_attachement_title()' +
+                '{0} in EthnologueParser.get_attachment_title()' +
                 ' sil:{1}'.format(type(e), self.sil))
 
-    def get_attachement(self, string):
+    def get_attachment(self, string):
         try:
-            attachement = string.split('<div class="attachment ' +
-            'attachment-after">')[1].split('<aside class=' +
-                     '"grid-6 region region-sidebar-second "id="' +
-                                          'region-sidebar-second">')[0]
-            return attachement
+            attachment = string.split(
+                '<div class="attachment attachment-after">')[1].split(
+                '<aside class="grid-6 region region-sidebar-second "id="' +
+                'region-sidebar-second">')[0]
+            return attachment
         except Exception as e:
             raise ParserException(
-            '{0} in EthnologueParser.get_attachement(), sil:{1}'
-                .format(type(e), self.sil))
+                '{0} in EthnologueParser.get_attachment(), sil:{1}'.format(
+                    type(e), self.sil))
 
-    def get_attachement_blocks_titles(self, attachement):
+    def get_attachment_blocks_titles(self, attachment):
         try:
-            attachement_blocks = attachement.split('<legend><span class=' +
-                                            '"fieldset-legend"><span>')[1:]
+            attachment_blocks = attachment.split(
+                '<legend><span class="fieldset-legend"><span>')[1:]
             attachment_titles = [block.split('</span')[0] for
-                                 block in attachement_blocks]
-            return attachement_blocks, attachment_titles
+                                 block in attachment_blocks]
+            return attachment_blocks, attachment_titles
         except Exception as e:
             raise ParserException(
-            '{0} in EthnologueParser.get_attachement_blocks_titles(),sil:{1}'
-                .format(type(e), self.sil))
+                '{0} in EthnologueParser.get_attachment_blocks_titles(),sil:{1}'.format(type(e), self.sil))  # nopep8
 
-    def get_attachement_dict(self, string):
-        attachement = self.get_attachement(string)
-        attachement_title = self.get_attachement_title(attachement)
-        if attachement_title is not None:
+    def get_attachment_dict(self, string):
+        attachment = self.get_attachment(string)
+        attachment_title = self.get_attachment_title(attachment)
+        if attachment_title is not None:
             return
-        attachement_dict = {}
-        blocks, titles =\
-                self.get_attachement_blocks_titles(attachement)
+        attachment_dict = {}
+        blocks, titles = self.get_attachment_blocks_titles(attachment)
         for i, block in enumerate(blocks):
             block_title = titles[i]
-            attachement_dict[block_title] = {}
-            inner_dictionary = self.parse_attachement_block(block)
+            attachment_dict[block_title] = {}
+            inner_dictionary = self.parse_attachment_block(block)
             for key in inner_dictionary:
-                attachement_dict[block_title][key]\
-                        = inner_dictionary[key]
-        return attachement_title, attachement_dict
+                attachment_dict[block_title][key] = inner_dictionary[key]
+        return attachment_title, attachment_dict
 
     def parse(self, sil_codes):
         for sil_code in sil_codes:
             self.sil = sil_code
             url = '{0}/{1}'.format(self.base_url, self.sil)
             html = get_html(url)
-            dictionary = {}
-            dictionary['Name'] = self.get_title(html)
+            dictionary = {'sil': sil_code}
+            dictionary['name'] = self.get_title(html)
             dictionary['Country'] = self.get_country(html)
             main_items = self.process_main_table_rows(html)
             if main_items is not None:
                 for i in main_items:
                     key, value = i
                     dictionary[key] = value
-            attachement_info = self.get_attachement_dict(html)
-            if attachement_info is not None:
-                t, dict_ = attachement_info
+            attachment_info = self.get_attachment_dict(html)
+            if attachment_info is not None:
+                t, dict_ = attachment_info
                 dictionary[t] = dict_
             yield dictionary
 
-{u'ISO 639-3': u'aaa', 'Name': u'Ghotuo', u'Classification': u'Niger-Congo, Atlantic-Congo, Volta-Congo, Benue-Congo, Edoid, North-Central, Ghotuo-Uneme-Yekhee', 'Country': u'Nigeria', None: {}, u'Location': u'Edo State, Owan East LGA, Ogbodo.', u'Language Maps': u'Nigeria, Map  6', u'Language Resources': u'OLAC resources in and about Ghotuo', u'Language Status': u'6a (Vigorous).', u'Population': u'9,000 (1994).'}
+{
+    u'ISO 639-3': u'aaa',
+    'Name': u'Ghotuo',
+    u'Classification': u'Niger-Congo, Atlantic-Congo, Volta-Congo, Benue-Congo, Edoid, North-Central, Ghotuo-Uneme-Yekhee',  # nopep8
+    'Country': u'Nigeria',
+    None: {},
+    u'Location': u'Edo State, Owan East LGA, Ogbodo.',
+    u'Language Maps': u'Nigeria, Map  6',
+    u'Language Resources': u'OLAC resources in and about Ghotuo',
+    u'Language Status': u'6a (Vigorous).',
+    u'Population': u'9,000 (1994).'
+}
+
+
 def main():
 
     sil_codes = sys.argv[1:]
