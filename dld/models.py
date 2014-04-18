a16126fbf9e93fb95a0a6b642047ab42dcbe31ba
zseder <zseder@ilab.sztaki.hu>
2014-04-18 12:25:40 +0200

    modified:   dld/models.py new crubadan fields

--- dld/models.py
+++ dld/models.py
@@ -11,6 +11,12 @@ class Language(models.Model):
     iso_type = models.CharField(max_length=100, blank=True)
     eth_status = models.CharField(max_length=100, blank=True)
     eth_population = models.IntegerField(blank=True, null=True)
+    cru_docs = models.IntegerField(blank=True, null=True)
+    cru_words = models.IntegerField(blank=True, null=True)
+    cru_characters = models.IntegerField(blank=True, null=True)
+    cru_floss_splchk = models.BooleanField(default=False)
+    cru_watchtower = models.BooleanField(default=False)
+    cru_udhr = models.BooleanField(default=False)
 
     def __unicode__(self):
         return u"{0} ({1})".format(self.name, self.sil)
