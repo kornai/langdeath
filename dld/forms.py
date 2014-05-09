45d27dd5c65d5febd6bf6037b4e597c2603fcbab
Judit Acs <judit@sch.bme.hu>
2014-05-09 15:15:28 +0200

    Replace name2 (old test field) with native_name. 	modified:   dld/forms.py

--- dld/forms.py
+++ dld/forms.py
@@ -2,17 +2,17 @@ from django.forms import ModelForm, CharField
 
 from dld.models import Language
 
+
 class LanguageSearchForm(ModelForm):
     name = CharField(required=False, label='English name')
-    name2 = CharField(required=False, label='Alternative name')
+    native_name = CharField(required=False, label='Alternative name')
     sil = CharField(required=False, label='SIL code')
 
     class Meta:
         model = Language
-        fields = ['name', 'name2', 'sil']
+        fields = ['name', 'native_name', 'sil']
 
     def get_languages(self):
-        return Language.objects.filter(name__contains=self.cleaned_data['name'],
-                                      name2__contains=self.cleaned_data['name2'],
-                                      sil__contains=self.cleaned_data['sil'])
-
+        return Language.objects.filter(name__contains=self.cleaned_data['name'].strip(),
+                native_name__contains=self.cleaned_data['native_name'].strip(),
+                sil__contains=self.cleaned_data['sil'].strip())
