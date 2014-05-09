c4347a54fb31cff159b47edac132aafc8c3ecb44
Judit Acs <judit@sch.bme.hu>
2014-05-09 15:25:09 +0200

    Search for alternative names 	modified:   dld/forms.py

--- dld/forms.py
+++ dld/forms.py
@@ -5,14 +5,16 @@ from dld.models import Language
 
 class LanguageSearchForm(ModelForm):
     name = CharField(required=False, label='English name')
-    native_name = CharField(required=False, label='Alternative name')
+    native_name = CharField(required=False, label='Native name')
+    alt_name = CharField(required=False, label='Alternative name')
     sil = CharField(required=False, label='SIL code')
 
     class Meta:
         model = Language
-        fields = ['name', 'native_name', 'sil']
+        fields = ['name', 'native_name', 'alt_name', 'sil']
 
     def get_languages(self):
         return Language.objects.filter(name__contains=self.cleaned_data['name'].strip(),
                 native_name__contains=self.cleaned_data['native_name'].strip(),
+                alt_name__name__contains=self.cleaned_data['alt_name'].strip(),
                 sil__contains=self.cleaned_data['sil'].strip())
