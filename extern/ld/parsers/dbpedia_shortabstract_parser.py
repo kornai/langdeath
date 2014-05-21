875a7f342adb2651e8eb11b602cae1f64a694d6c
Katalin <pajkossyk@gmail.com>
2014-05-21 16:56:52 +0200

    new file:   dbpedia_dump_parser.py 	modified:   dbpedia_shortabstract_parser.py kb flake8

--- extern/ld/parsers/dbpedia_shortabstract_parser.py
+++ extern/ld/parsers/dbpedia_shortabstract_parser.py
@@ -4,21 +4,21 @@ import re
 class DbpediaShortAbstractsParser(OfflineParser):
 
     def __init__(self, basedir='dbpedia_dumps'):
-        
+
         self.basedir = basedir
-        self.fh = open('{0}/short_abstracts_en.nt'\
+        self.fh = open('{0}/short_abstracts_en.nt'
                        .format(self.basedir))
         self.patterns = self.compile_patterns()
-    
+
     def language_from_title(self, url):
 
         m = self.patterns['lang_in_url'].search(url)
-        if m == None:
+        if m is None:
             return
         return m.groups()[0]
-    
+
     def compile_patterns(self):
-        
+
         patterns = {}
         patterns['lang_in_url'] = re.compile(r'.*/(.*?)_language>')
 
@@ -28,30 +28,29 @@ class DbpediaShortAbstractsParser(OfflineParser):
                            '[^A-Za-z]or[^A-Za-z]', '[^A-Za-z]\u2013[^A-Za-z]']
         patterns['splitters'] =\
                 re.compile('|'.join([w for w in splitter_phrases]))
-                               
+
         words_to_remove_from_pattern = \
-                ['also', 'known', 'as', 'and', 'in', 'less', 'preciseley', 
+                ['also', 'known', 'as', 'and', 'in', 'less', 'preciseley',
                  'called', 'formerly', 'literally', 'spelled', 'often',
-                 'abbreviated', 'script', 'transliterated', 
-                 'rarely', 'sometimes', 'common', 'form', 'autonym', 
+                 'abbreviated', 'script', 'transliterated',
+                 'rarely', 'sometimes', 'common', 'form', 'autonym',
                  'after', 'a', 'particular', 'speakers',
-                'dialect', 'precisely', 'alternatively', 'plural', 'archaically', 
-                'many', 'other', 'spellings', 'see', 'below', 'proper', 
-                'disambiguation', 'with', 'full', 'diactritics', 'written', 'names',
-                'former', 'ruling', 'clan', 'by', 'linguistics', 'etc.', 'to'
-                'be', 'exact']
+                 'dialect', 'precisely', 'alternatively', 'plural',
+                 'archaically', 'many', 'other', 'spellings', 'see', 'below',
+                 'proper', 'disambiguation', 'with', 'full', 'diactritics',
+                 'written', 'names', 'former', 'ruling', 'clan', 'by',
+                 'linguistics', 'etc.', 'to', 'be', 'exact']
 
         patterns['words_to_remove'] = \
-                re.compile('|'.join(['([^A-Za-z]|^)'+ w + '([^A-Za-z]|$)'
-                          for w in words_to_remove_from_pattern])) 
-        
+                re.compile('|'.join(['([^A-Za-z]|^)' + w + '([^A-Za-z]|$)'
+                          for w in words_to_remove_from_pattern]))
+
         patterns['alt_name'] = re.compile(
-            u'(^[\u03df-\u09FF\u0A01-\uffff]+)\s([\u0000-\u03DE]+)$|' + 
+            u'(^[\u03df-\u09FF\u0A01-\uffff]+)\s([\u0000-\u03DE]+)$|' +
             u'(^[\u0000-\u03DE]+)\s([\u03df-\u09FF\u0A01-\uffff]+)$')
 
         patterns['post_filter'] = re.compile('not\sto\sbe\sconfused\swith')
         return patterns
-    
 
     def postprocess(self, found_langs, language):
 
@@ -66,17 +65,17 @@ class DbpediaShortAbstractsParser(OfflineParser):
             if l != language and l != '' and \
                self.patterns['post_filter'].match(l) is None\
                and len(l.split()) < 4:
-                found_langs_2.add(l)   
-        return found_langs_2        
+                found_langs_2.add(l)
+        return found_langs_2
 
     def multiple_replacement(self, orig_string, pat):
-        
+
         m = pat.search(orig_string)
         string = orig_string
         while m is not None:
             string = re.sub(pat, ' ', string)
             m = pat.search(string)
-        return string    
+        return string
 
     def parse_alternatives(self, language, abstract):
 
@@ -91,17 +90,15 @@ class DbpediaShortAbstractsParser(OfflineParser):
                 m = self.patterns['alt_name'].search(lang)
                 if m is not None:
                     for i in m.groups():
-                        
                         if i is not None:
                             found_langs.add(i.strip())
-                else:            
-                    found_langs.add(lang)   
+                else:
+                    found_langs.add(lang)
             found_langs = self.postprocess(found_langs, language)
             return list(found_langs)
         else:
             return []
 
-    
     def parse(self):
 
         for l in self.fh:
@@ -113,8 +110,9 @@ class DbpediaShortAbstractsParser(OfflineParser):
                     continue
                 alternatives = self.parse_alternatives(language, abstract)
                 if len(alternatives) > 0:
-                    yield {u'name': language, u'altnames': alternatives}
-           
+                    yield {u'name': language, u'altname': alternatives}
+
+
 def main():
 
     parser = DbpediaShortAbstractsParser()
