e829a1735f8372c5bdf45981c4eb081ee57f5fa0
zseder <zseder@ilab.sztaki.hu>
2014-05-21 17:53:49 +0200

    modified:   extern/ld/parsers/software_support_parser.py code clarification

--- extern/ld/parsers/software_support_parser.py
+++ extern/ld/parsers/software_support_parser.py
@@ -10,21 +10,25 @@ class SoftwareSupportParser(TSV_parser):
 
     def parse(self):
         parser = TSV_parser()
-        langs = defaultdict(lambda: {
-            'mac_input': False,
-            'mac_input_partial': False,
-            'microsoft_pack': False})
-        for langiter in [
-            parser.parse('{0}/mac_input'.format(self.resdir),
-                         true_key=['mac_input', 'mac_input_partial']),
-            parser.parse('{0}/mac_input_partial_support'.format(self.resdir),
-                         true_key=['mac_input_partial']),
-            parser.parse('{0}/win8_pack'.format(self.resdir),
-                         true_key=['microsoft_pack']),
-            parser.parse('{0}/win8_input_option'.format(self.resdir),
-                         true_key=['win8_input_method']),
-            parser.parse('{0}/office_if_pack'.format(self.resdir),
-                         field_target={0: 'name', 5: 'office_if_2013'})]:
-            for lang in langiter:
+        langs = defaultdict(lambda: {'mac_input': False,
+                                     'mac_input_partial': False,
+                                     'microsoft_pack': False})
+
+        parameters = [
+            ('{0}/mac_input'.format(self.resdir),
+             {"true_key": ['mac_input', 'mac_input_partial']}),
+            ('{0}/mac_input_partial_support'.format(self.resdir),
+             {"true_key": ['mac_input_partial']}),
+            ('{0}/win8_pack'.format(self.resdir),
+             {"true_key": ['microsoft_pack']}),
+            ('{0}/win8_input_option'.format(self.resdir),
+             {"true_key": ['win8_input_method']}),
+            ('{0}/office_if_pack'.format(self.resdir),
+             {"field_target": {0: 'name', 5: 'office_if_2013'}})
+        ]
+        for args in parameters:
+            for lang in parser.parse(args[0], **args[1]):
                 langs[lang['name']].update(lang)
-        return langs
+
+        for lang in langs.itervalues():
+            yield lang
