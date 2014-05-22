b4623c9ef1c054fc100d079a58db8bf634af447b
Katalin <pajkossyk@gmail.com>
2014-05-22 17:17:55 +0200

    modified:   dbpedia_shortabstract_parser.py further improvements

--- extern/ld/parsers/dbpedia_shortabstract_parser.py
+++ extern/ld/parsers/dbpedia_shortabstract_parser.py
@@ -23,25 +23,35 @@ class DbpediaShortAbstractsParser(OfflineParser):
         patterns['lang_in_url'] = re.compile(r'.*/(.*?)_language>')
 
         splitter_phrases =\
-                ['in\s[A-Za-z]+\sscript', 'in\s[A-Za-z]+\sdialect',
-                           '[A-Z][a-z]*:', ';', ',', '/', '~',
-                           '[^A-Za-z]or[^A-Za-z]', '[^A-Za-z]\u2013[^A-Za-z]']
+                ['in\s[A-Za-z]+\s', '[A-Z][a-z]*:', ';', ',', '/', '~',
+                           '[^A-Za-z]or[^A-Za-z]', '[^A-Za-z]\u2013[^A-Za-z]',
+                'in\s.*?\sdialect', 'in\s.*?\sscript', 'in\s.*?\slanguage']
         patterns['splitters'] =\
                 re.compile('|'.join([w for w in splitter_phrases]))
 
         words_to_remove_from_pattern1 = \
                 ['also', 'known', 'as', 'and', 'in', 'less', 'preceisely',
                  'called', 'formerly', 'literally', 'spelled', 'often',
-                 'abbreviated', 'script', 'transliterated',
+                 'abbreviated', 'script', 'transliterated', 'spelling',
                  'rarely', 'sometimes', 'common', 'form', 'autonym',
-                 'after', 'a', 'particular', 'speakers']
+                 'after', 'a', 'particular', 'speakers', 'contrasting']
 
         words_to_remove_from_pattern2 = \
-                ['dialect', 'precisely', 'alternatively', 'plural',
+                ['dialect', 'precisely', 'alternatively', 'plural', 'name',
                 'archaically', 'many', 'other', 'spellings', 'see', 'below',
                 'proper', 'disambiguation', 'with', 'full', 'diactritics',
                 'written', 'names', 'former', 'ruling', 'clan', 'by',
-                'linguistics', 'etc.', 'to', 'be', 'exact']
+                'linguistics', 'etc.', 'to', 'be', 'exact', 'rendered',
+                'of', 'the']
+
+        words_to_remove_from_pattern3 = \
+                ['all', 'alternate', 'anglicised', 'autoethnonym', 'commonly',
+                 'dialects', 'earlier', 'easier', 'elsewhere',
+                 'endonym', 'erroneously', 'ethnonym', 'from', 'house',
+                 'including', 'just', 'local', 'locally', 'means',
+                 'misspelled', 'native', 'natively', 'obsolete',
+                 'otherwise', 'pronounced', 'recorded', 'referred', 'rendered',
+                 's', 'spelt', 'their', 'variety', 'variously', 'we']
 
         patterns['words_to_remove1'] = \
                 re.compile('|'.join(['([^A-Za-z]|^)' + w + '([^A-Za-z]|$)'
@@ -51,11 +61,20 @@ class DbpediaShortAbstractsParser(OfflineParser):
                 re.compile('|'.join(['([^A-Za-z]|^)' + w + '([^A-Za-z]|$)'
                           for w in words_to_remove_from_pattern2]))
 
+        patterns['words_to_remove3'] = \
+                re.compile('|'.join(['([^A-Za-z]|^)' + w + '([^A-Za-z]|$)'
+                          for w in words_to_remove_from_pattern3]))
+
         patterns['alt_name'] = re.compile(
             u'(^[\u03df-\u09FF\u0A01-\uffff]+)\s([\u0000-\u03DE]+)$|' +
             u'(^[\u0000-\u03DE]+)\s([\u03df-\u09FF\u0A01-\uffff]+)$')
 
-        patterns['post_filter'] = re.compile('not\sto\sbe\sconfused\swith')
+        patterns['first_filter'] = re.compile(
+            '(not\sto\sbe\sconfused\swith\s|contrasting|so named after the)' +
+            '.*?([,;]|$)')
+
+        patterns['language_of_the'] = re.compile(
+            '(language\sof\sthe[A-Z][a-z]+|language\sof\s[A-Z][a-z]+)')
         return patterns
 
     def postprocess(self, found_langs, language):
@@ -64,15 +83,23 @@ class DbpediaShortAbstractsParser(OfflineParser):
         for l in found_langs:
             if l[:3] == 'or ':
                 l = l[3:]
+
+            if self.patterns['language_of_the'].search(l) is not None:
+                found_langs_2.add(self.patterns['language_of_the']
+                                  .search(l).groups()[0])
+                continue
+
             l = self.multiple_replacement(l, self.patterns['words_to_remove1'])
             l = self.multiple_replacement(l, self.patterns['words_to_remove2'])
+            l = self.multiple_replacement(l, self.patterns['words_to_remove3'])
             l = re.sub('[0-9]*', '', l)
             l = l.strip()
             l = l.strip('"')
-            if l != language and l != '' and \
-               self.patterns['post_filter'].match(l) is None\
-               and len(l.split()) < 4:
+
+            if l != language and l != '' and len(l.split()) < 4 and\
+               l != '{0} language'.format(language):
                 found_langs_2.add(l)
+
         return found_langs_2
 
     def multiple_replacement(self, orig_string, pat):
@@ -87,11 +114,18 @@ class DbpediaShortAbstractsParser(OfflineParser):
     def parse_alternatives(self, language, abstract):
 
         found_langs = set([])
+        first_or_pattern =\
+                re.compile(language + u'\sor([A-Za-z\s]+)\sis')
+        first_or = first_or_pattern.search(abstract)
+        if first_or is not None:
+            return [first_or.groups()[0]]
         first_bracket_pattern =\
                 re.compile(language + '(\slanguage){0,1}[\s]{0,1}\((.*?)\)')
         first_bracket = first_bracket_pattern.search(abstract)
         if first_bracket is not None:
             bracket_string = first_bracket.groups()[1]
+            bracket_string = re.sub(self.patterns['first_filter'],
+                                    '', bracket_string)
             for lang in re.split(self.patterns['splitters'], bracket_string):
                 lang = lang.strip()
                 m = self.patterns['alt_name'].search(lang)
