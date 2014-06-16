2f991c9bb38c9e3bd4b4ce4253fee5096e3e9285
Judit Acs <judit@sch.bme.hu>
2014-06-16 16:21:26 +0200

    Remove basedir 	modified:   base_parsers.py

--- extern/ld/parsers/base_parsers.py
+++ extern/ld/parsers/base_parsers.py
@@ -1,23 +1,19 @@
 import cPickle
-from os import path
 
 
 class BaseParser(object):
-
-    def __init__(self, basedir=''):
-        self.basedir = basedir
 
     @property
     def pickle_fn(self):
         return type(self).__name__ + '.pickle'
 
     def load_from_file(self, ifn=None):
-        fn = ifn if ifn else path.join(self.basedir, self.pickle_fn)
+        fn = ifn if ifn else self.pickle_fn
         with open(fn) as f:
             return cPickle.load(f)
 
     def dump_to_file(self, ofn=None):
-        fn = ofn if ofn else path.join(self.basedir, self.pickle_fn)
+        fn = ofn if ofn else self.pickle_fn
         with open(fn, 'w') as f:
             lang_data = list(self.parse_all())
             f.write(cPickle.dumps(lang_data))
