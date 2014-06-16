72b07baaae5e4f1003f932cfa45c7f96f98a4e02
Judit Acs <judit@sch.bme.hu>
2014-06-16 15:47:31 +0200

    Serializer and deserializer methods in the base class. 	modified:   base_parsers.py

--- extern/ld/parsers/base_parsers.py
+++ extern/ld/parsers/base_parsers.py
@@ -1,10 +1,36 @@
+import cPickle
+from os import path
+
+
 class BaseParser(object):
-    pass
+
+    def __init__(self, basedir=''):
+        self.basedir = basedir
+
+    @property
+    def pickle_fn(self):
+        return type(self).__name__ + '.pickle'
+
+    def load_from_file(self, ifn=None):
+        fn = ifn if ifn else path.join(self.basedir, self.pickle_fn)
+        with open(fn) as f:
+            return cPickle.load(f)
+
+    def dump_to_file(self, ofn=None):
+        fn = ofn if ofn else path.join(self.basedir, self.pickle_fn)
+        with open(fn, 'w') as f:
+            lang_data = list(self.parse_all())
+            f.write(cPickle.dumps(lang_data))
+
+    def parse_all(self):
+        return []
+
 
 class OnlineParser(BaseParser):
     """Inherit this class if the parser will do everything on its own,
     downloading, parsing, no assistance, supplementary files are needed
     """
+
 
 class OfflineParser(BaseParser):
     """Inherit this class if the parser will need some extern files that
