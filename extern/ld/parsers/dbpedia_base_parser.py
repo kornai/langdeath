8dd44b8c6c6d41f4ac00d4fd1ca320673481a391
zseder <zseder@ilab.sztaki.hu>
2014-06-17 12:55:52 +0200

    modified:   extern/ld/parsers/dbpedia_base_parser.py value and key cleaning

--- extern/ld/parsers/dbpedia_base_parser.py
+++ extern/ld/parsers/dbpedia_base_parser.py
@@ -1,3 +1,5 @@
+import urllib
+
 from base_parsers import OfflineParser
 
 
@@ -15,6 +17,8 @@ class DbpediaNTBaseParser(OfflineParser):
                 continue
             data = l.strip('\n').decode('unicode_escape').split(' ', 2)
             page, key, value = data
+            value = self.clean_value(value)
+            key = self.clean_key(key)
             page = page.split('/')[4].split('>')[0]
             if page not in self.needed_titles:
                 if len(actual_lang) > 0:
@@ -25,6 +29,12 @@ class DbpediaNTBaseParser(OfflineParser):
             name = self.get_language_from_title(page)
             if len(actual_lang) == 0:
                 actual_lang['name'] = name
+            else:
+                if name != actual_lang['name']:
+                    yield actual_lang
+                    actual_lang = {}
+                    actual_lang['name'] = name
+                    continue
 
             actual_lang.setdefault(key, []).append(value)
         if len(actual_lang) > 0:
@@ -35,5 +45,22 @@ class DbpediaNTBaseParser(OfflineParser):
         if words[-1] in ['language', 'languages']:
             return ' '.join(words[:-1])
         return ' '.join(words)
+
+    def clean_key(self, key):
+        if "<http://dbpedia.org/ontology/" in key:
+            return urllib.unquote(key.split("/")[-1].split(">")[0])
+        else:
+            return key
+    
+    def clean_value(self, value):
+        if "@en" in value:
+            return value[1:-6]
+        elif "http://dbpedia.org/resource/" in value:
+            return self.get_language_from_title(
+                urllib.unquote(value.split("/")[-1].split(">")[0]))
+        elif "<http://www.w3.org/2001/XMLSchema#string>" in value:
+            return value.split('"')[1]
+        else:
+            return value
 
 
