96a2e614f1329d2c048ae9d3a8537f08e240579c
makrai <makraimarton@gmail.com>
2014-06-18 14:51:57 +0200

    splitted to more functions

--- extern/ld/parsers/wp_tlds.py
+++ extern/ld/parsers/wp_tlds.py
@@ -9,20 +9,17 @@ import logging
 from base_parsers import OnlineParser
 
 class WikipediaToplevelDomainParser(OnlineParser):
-    def tlds(self, wp_code):
-        """
-        returns a language update with the Wikipedia code and the frequencies
-        of top level domains (TLD) in external links in the corresponding
-        Wikipedia. TLD cutoff: 1 % of all links
-        """
-        tld_freq = defaultdict(int)
+    def get_file(self, wp_code):
         url = 'http://dumps.wikimedia.org/'+wp_code+'wiki/latest/'+wp_code+'wiki-latest-externallinks.sql.gz'
         request = urllib2.Request(url)
         request.add_header('Accept-encoding', 'gzip')
         response = urllib2.urlopen(request)
         buf = StringIO( response.read())
-        f = gzip.GzipFile(fileobj=buf)
-        for line in f.readlines():
+        self.file_ =  gzip.GzipFile(fileobj=buf)
+
+    def count_links(self):
+        self.tld_freq = defaultdict(int)
+        for line in self.file_.readlines():
             # parts copied from
             # hunmisc/hunmisc/wikipedia/parse_insert_into_rows.py
             if not line.startswith("INSERT INTO"):
@@ -40,19 +37,32 @@ class WikipediaToplevelDomainParser(OnlineParser):
                     if '//' in ext_url:
                         domain = ext_url.split('/')[2]
                         tld = domain.split('.')[-1]
-                        tld_freq[tld] += 1
+                        self.tld_freq[tld] += 1
                 except UnicodeDecodeError, ude:
                     sys.stderr.write(
                         "There is an encoding problem with a link:\n")
                     sys.stderr.write(
                         str(entry).decode("utf-8", "ignore") + "\n")
-        total = sum(tld_freq.itervalues())
+
+    def postproc_counts(self, wp_code):
+        total = sum(self.tld_freq.itervalues())
         tld_freq = dict(
             (k, v) 
-            for k, v in tld_freq.iteritems() if 100 * v > total)
-        return {'wp_code': wp_code, 'wp_tlds': sorted(
+            for k, v in self.tld_freq.iteritems() if 100 * v > total)
+        self.tld_freq = {'wp_code': wp_code, 'wp_tlds': sorted(
             tld_freq.iteritems(), key=operator.itemgetter(1),
             reverse=True)}
+
+    def tlds(self, wp_code):
+        """
+        returns a language update with the Wikipedia code and the frequencies
+        of top level domains (TLD) in external links in the corresponding
+        Wikipedia. TLD cutoff: 1 % of all links
+        """
+        self.get_file(wp_code)
+        self.count_links()
+        self.postproc_counts(wp_code)
+        return self.tld_freq
 
     def parse(self):
         wp_codes = [ 
