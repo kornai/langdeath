a90e5e7148d10bcdd28a9222bf4afed7857e6c35
makrai <makraimarton@gmail.com>
2014-06-19 15:40:10 +0200

    lang codes taken from list_of_wikipedia_parser.py

--- extern/ld/parsers/wp_tlds.py
+++ extern/ld/parsers/wp_tlds.py
@@ -7,6 +7,7 @@ import operator
 import logging
 
 from base_parsers import OnlineParser
+from list_of_wikipedia_parser import WikipediaListOfLanguagesParser
 
 class WikipediaToplevelDomainParser(OnlineParser):
     def get_file(self, wp_code):
@@ -65,38 +66,9 @@ class WikipediaToplevelDomainParser(OnlineParser):
         return self.tld_freq
 
     def parse(self):
-        wp_codes = [
-            'aa', 'ab', 'ace', 'af', 'ak', 'als', 'am', 'an', 'ang', 'ar',
-            'arc', 'arz', 'as', 'ast', 'av', 'ay', 'az', 'ba', 'bar',
-            'bat-smg', 'bcl', 'be', 'be-x-old', 'bg', 'bh', 'bi', 'bjn', 'bm',
-            'bn', 'bo', 'bpy', 'br', 'bs', 'bug', 'bxr', 'ca', 'cbk-zam',
-            'cdo', 'ce', 'ceb', 'ch', 'cho', 'chr', 'chy', 'ckb', 'co', 'cr',
-            'crh', 'cs', 'csb', 'cu', 'cv', 'cy', 'da', 'de', 'diq', 'dsb',
-            'dv', 'dz', 'ee', 'el', 'eml', 'en', 'eo', 'es', 'et', 'eu',
-            'ext', 'fa', 'ff', 'fi', 'fiu-vro', 'fj', 'fo', 'fr', 'frp',
-            'frr', 'fur', 'fy', 'ga', 'gag', 'gan', 'gd', 'gl', 'glk', 'gn',
-            'got', 'gu', 'gv', 'ha', 'hak', 'haw', 'he', 'hi', 'hif', 'ho',
-            'hr', 'hsb', 'ht', 'hu', 'hy', 'hz', 'ia', 'id', 'ie', 'ig', 'ii',
-            'ik', 'ilo', 'io', 'is', 'it', 'iu', 'ja', 'jbo', 'jv', 'ka',
-            'kaa', 'kab', 'kbd', 'kg', 'ki', 'kj', 'kk', 'kl', 'km', 'kn',
-            'ko', 'koi', 'kr', 'krc', 'ks', 'ksh', 'ku', 'kv', 'kw', 'ky',
-            'la', 'lad', 'lb', 'lbe', 'lez', 'lg', 'li', 'lij', 'lmo', 'ln',
-            'lo', 'lt', 'ltg', 'lv', 'map-bms', 'mdf', 'mg', 'mh', 'mhr',
-            'mi', 'min', 'mk', 'ml', 'mn', 'mo', 'mr', 'mrj', 'ms', 'mt',
-            'mus', 'mwl', 'my', 'myv', 'mzn', 'na', 'nah', 'nap', 'nds',
-            'nds-nl', 'ne', 'new', 'ng', 'nl', 'nn', 'no', 'nov', 'nrm',
-            'nso', 'nv', 'ny', 'oc', 'om', 'or', 'os', 'pa', 'pag', 'pam',
-            'pap', 'pcd', 'pdc', 'pfl', 'pi', 'pih', 'pl', 'pms', 'pnb',
-            'pnt', 'ps', 'pt', 'qu', 'rm', 'rmy', 'rn', 'ro', 'roa-rup',
-            'roa-tara', 'ru', 'rue', 'rw', 'sa', 'sah', 'sc', 'scn', 'sco',
-            'sd', 'se', 'sg', 'sh', 'si', 'simple', 'sk', 'sl', 'sm', 'sn',
-            'so', 'sq', 'sr', 'srn', 'ss', 'st', 'stq', 'su', 'sv', 'sw',
-            'szl', 'ta', 'te', 'tet', 'tg', 'th', 'ti', 'tk', 'tl', 'tn',
-            'to', 'tpi', 'tr', 'ts', 'tt', 'tum', 'tw', 'ty', 'tyv', 'udm',
-            'ug', 'uk', 'ur', 'uz', 've', 'vec', 'vep', 'vi', 'vls', 'vo',
-            'wa', 'war', 'wo', 'wuu', 'xal', 'xh', 'xmf', 'yi', 'yo', 'za',
-            'zea', 'zh', 'zh-classical', 'zh-min-nan', 'zh-yue', 'zu']
-        for wp_code in wp_codes:
+        list_parser = WikipediaListOfLanguagesParser()
+        for lang_dict in list_parser.parse():
+            wp_code = lang_dict['Wiki']
             try:
                 yield self.tlds(wp_code)
             except urllib2.HTTPError as e:
