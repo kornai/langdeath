07c0b643da5fb22ccd6132afdbf41a74846b2cea
Katalin <pajkossyk@gmail.com>
2014-06-19 15:50:29 +0200

    modified:   wpsize_counter.py

--- extern/ld/parsers/wpsize_counter.py
+++ extern/ld/parsers/wpsize_counter.py
@@ -5,33 +5,49 @@ from math import log
 from collections import defaultdict
 import re
 import string
+import gzip
 
 
 class WikipediaAdjustedSizeCounter():
 
-    def __init__(self, min_chars=50):
+    def __init__(self, basic_limit=2000, entropy_sample_lines=50000):
 
         self.numerals = set(string.digits)
         self.punctuation = set(string.punctuation)
         self.compile_regexes()
-        self.min_chars = min_chars
+        self.basic_limit = basic_limit
+        self.entropy_sample_lines = entropy_sample_lines
 
     def compile_regexes(self):
 
         self.title_pattern = re.compile('^%%#PAGE (.*)')
 
-    def get_char_counts(self, data):
+    def get_char_counts_of_all(self, data):
 
         d = defaultdict(int)
-        for real_page in self.generate_real_pages(data):
-            for l in real_page:
+        for is_real, page in self.generate_pages(data, 0):
+            for l in page:
                 l = l.lower()
                 for ch in l:
                     if ch not in self.numerals and ch not in self.punctuation:
                         d[ch] += 1
         return d
 
-    def generate_real_pages(self, data):
+    def get_char_length_of_real(self, data, min_chars):
+
+        c = 0
+        stub_count = 0
+        article_count = 0
+        is_real = False
+        for is_real, page in self.generate_pages(data, min_chars):
+            if is_real is True:
+                article_count += 1
+                c += len(page)
+            else:
+                stub_count += 1
+        return c, article_count, stub_count
+
+    def generate_pages(self, data, min_chars):
 
         page = []
         for l in data:
@@ -39,38 +55,63 @@ class WikipediaAdjustedSizeCounter():
             title_matched = self.title_pattern.match(l)
             if title_matched is not None:
                 char_counts = sum([len(l) for l in page])
-                if char_counts > self.min_chars:
-                    yield page
+                yield char_counts > min_chars, page
                 l = title_matched.groups()[0]
                 page = []
             page.append(l)
         char_counts = sum([len(l) for l in page])
-        if char_counts > self.min_chars:
-            yield page
+        yield char_counts > min_chars, page
 
     def calculate_entropy(self, values):
 
         sum_ = sum(values)
-        entropy = log(sum_)/log(2) - sum([v * (log(v)/log(2))/sum_
-                                          for v in values])
-        return entropy
+        e = log(sum_)/log(2) - sum([v * (log(v)/log(2))/sum_
+                                    for v in values])
+        return e
 
-    def count(self, data):
+    def get_sample(self, f):
 
-        d = self.get_char_counts(data)
-        char_freqs = d.values()
-        wp_size = sum(char_freqs)
-        char_entropy = self.calculate_entropy(char_freqs)
-        adjusted_size = wp_size * char_entropy
-        return wp_size, char_entropy, adjusted_size
+        sample = []
+        i = 0
+        for l in f:
+            sample.append(l)
+            i += 1
+            if i > self.entropy_sample_lines:
+                return sample
+
+    def count_wp_size_from_file(self, data_file, stub_limit):
+
+        f = gzip.open(data_file)
+        c, a, s = self.get_char_length_of_real(f, stub_limit)
+        f.close()
+        return c, a, s
+
+    def count_entropy_from_file(self, data_file):
+
+        f = gzip.open(data_file)
+        f_sample = self.get_sample(f)
+        d = self.get_char_counts_of_all(f_sample)
+        values = d.values()
+        e = self.calculate_entropy(values)
+        stub_limit = self.basic_limit/e
+        f.close()
+        return e, stub_limit
+
+    def count(self, data_file):
+
+        e, stub_limit = self.count_entropy_from_file(data_file)
+        wp_size, article_count, stub_count =\
+            self.count_wp_size_from_file(data_file, stub_limit)
+        adjusted_size = wp_size * e
+        return wp_size, e, stub_limit, article_count, stub_count, adjusted_size
 
 
 def main():
 
-    data = sys.stdin
+    data = sys.argv[1]
     a = WikipediaAdjustedSizeCounter()
-    size, entropy, adjusted_size = a.count(data)
-    print size, entropy, adjusted_size
+    print a.count(data)
+
 
 if __name__ == "__main__":
     main()
