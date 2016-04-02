457fad76c4d79ccae8160f714c6cd959570ca7ca
Katalin Pajkossy <pajkossy@info.ilab.sztaki.hu>
2016-04-02 23:15:41 +0200

    l1 feature selection, stats printed separately for all and best crossvalidating models

--- classification/classifier.py
+++ classification/classifier.py
@@ -6,17 +6,18 @@ from collections import defaultdict, Counter
 from sklearn.linear_model import LogisticRegression
 from sklearn import cross_validation
 from sklearn.feature_selection import SelectFromModel
+import numpy
 
 class Classifier:
 
-    def __init__(self, tsv, exp_count, threshold, classcount, limit):
+    def __init__(self, tsv, exp_count, classcount, limit):
 
         self.df = pandas.read_csv(tsv, sep='\t')
-        self.df_res = self.df[['sil']].set_index('sil')
+        series = ['crossval_res'] + self.df['sil'].tolist()
+        self.df_res = pandas.DataFrame(index=series)
         self.df = self.df.set_index(u'sil')
         self.all_feats = self.df.drop('seed_label', axis=1)
         self.exp_count = exp_count
-        self.thr = threshold
         self.classes = classcount
         self.limit = limit
 
@@ -70,16 +71,17 @@ class Classifier:
             scores, sum(scores)/5))
         return sum(scores)/5
 
-    def get_selector(self, threshold=2):
-        model = LogisticRegression()
-        self.selector = SelectFromModel(model, threshold=threshold)
+    def get_selector(self):
+        selector_model = LogisticRegression(penalty='l1')
+        self.selector = SelectFromModel(selector_model)
         self.selector.fit(self.feats, self.labels)
         support = self.selector.get_support(indices=True)
         logging.info('selected features:{}'.format(
             self.df.iloc[:, support].keys()))
 
 
-    def train_label(self, selector=None, label='exp'):
+    def train_label(self, crossval_res=0.0, selector=None,
+                    label='exp'):
         model = LogisticRegression()
         train_data = self.feats
         all_data = self.all_feats
@@ -87,7 +89,7 @@ class Classifier:
             train_data = selector.transform(train_data)
             all_data = selector.transform(self.all_feats)
         model.fit(train_data, self.labels)
-        self.df_res[label] = model.predict(all_data)
+        self.df_res[label] = [crossval_res] +  list(model.predict(all_data))
         logging.info('labelings:\n{}'.format(pandas.value_counts(
         self.df_res[label].values)))
     
@@ -101,7 +103,7 @@ class Classifier:
                 d2['+'] += d[k]
         if d2['-'] < 3:
             return 'living'
-        if d2['+'] < 3:
+        if d2['+']  < 3:
             return 'still'
         else:
             return 'borderline'
@@ -110,18 +112,26 @@ class Classifier:
         for i in range(self.exp_count):
             self.get_train_df()
             crossval_res = self.train_crossval()
-            if crossval_res > self.limit:
-                self.train_label(label='exp_full_features_{0}'.format(i))
-            self.get_selector(threshold=self.thr)
+            self.train_label(crossval_res=crossval_res,
+                             label='exp_full_features_{0}'.format(i))
+            self.get_selector()
             crossval_res = self.train_crossval(selector=self.selector)
-            if crossval_res > self.limit:
-                self.train_label(selector=self.selector,
+            self.train_label(crossval_res=crossval_res, selector=self.selector,
                          label='exp_with_feature_sel_{0}'.format(i))
         self.df_res['status'] = self.df_res.apply(lambda x:Counter(x),
                                                   axis=1).apply(self.map_values)
+        #best = self.df_res.loc[:, self.df_res['crossval_res'] > self.limit]
+        needed = self.df_res.iloc[0] > self.limit
+        needed_list = numpy.where(needed.tolist())[0].tolist()
+        best = self.df_res.iloc[:, needed_list]
+        self.df_res['status_best'] = best.apply(lambda x:Counter(x), axis=1).apply(self.map_values)
         if out_fn != None:
+            print self.df_res.status.value_counts()
+            print best.shape
+            print self.df_res.status_best.value_counts()
             logging.info('exporting dataframe to {}'.format(out_fn))  
             self.df_res.to_csv(out_fn, sep='\t', encoding='utf-8')
+
       
             
 
@@ -133,13 +143,12 @@ def main():
         "%(module)s (%(lineno)s) - %(levelname)s - %(message)s")
     preprocessed_tsv = sys.argv[1]
     exp_count = int(sys.argv[2])
-    threshold = float(sys.argv[3])
-    classcount = int(sys.argv[4])
-    limit = float(sys.argv[5])
+    classcount = int(sys.argv[3])
+    limit = float(sys.argv[4])
     out_fn = None
-    if len(sys.argv) > 6:
-        out_fn = sys.argv[6]
-    a = Classifier(preprocessed_tsv, exp_count, threshold, classcount, limit)
+    if len(sys.argv) > 5:
+        out_fn = sys.argv[5]
+    a = Classifier(preprocessed_tsv, exp_count, classcount, limit)
     a.train_classify(out_fn)
 
 if __name__ == '__main__':
