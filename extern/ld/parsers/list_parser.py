663425ae17698975c88cae158126ec52a012765d
Katalin Pajkossy <pajkossy@info.ilab.sztaki.hu>
2016-06-01 16:12:52 +0200

    field for languages for siren project

--- extern/ld/parsers/list_parser.py
+++ extern/ld/parsers/list_parser.py
@@ -17,10 +17,16 @@ class LeibzigCorporaParser(ListParser):
     def __init__(self, fn):
         super(LeibzigCorporaParser, self).__init__(
             fn, 'name', 'in_leipzig_corpora')
+
+class SirenLanguagesParser(ListParser):
+    def __init__(self, fn):
+        super(SirenLanguagesParser, self).__init__(
+            fn, 'name', 'in_siren_project')
+
         
 def main():
     import sys
-    a = LeibzigCorporaParser(sys.argv[1])
+    a = SirenLanguagesParser(sys.argv[1])
     for d in a.parse():
         print d
 
